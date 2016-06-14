cf249340ead7c7bb7fa730b752b277e5f30d0400
root <root@debian>
2016-06-14 20:41:13 +0800

    add intenal method

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+// Template.postSubmit.events({
+//   'submit form': function(e) {
+//     e.preventDefault();
+
+//     var post = {
+//       url: $(e.target).find('[name=url]').val(),
+//       title: $(e.target).find('[name=title]').val()
+//     };
+
+//     post._id = Posts.insert(post);
+//     Router.go('postPage', post);
+//   }
+// });
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,7 +20,12 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+    Meteor.call('postInsert', post, function(error, result) {
+      // 显示错误信息并退出
+      if (error)
+        return alert(error.reason);
+
+      Router.go('postPage', {_id: result._id});
+    });
   }
 });
