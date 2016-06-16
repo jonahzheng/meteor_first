95e22438c4a45f765275ce2818bbfcc40a0d6684
root <root@debian>
2016-06-16 17:30:34 +0800

    add error model

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -23,14 +23,13 @@ Template.postSubmit.events({
     Meteor.call('postInsert', post, function(error, result) {
       // 显示错误信息并退出
       if (error)
-        return alert(error.reason);
+        return throwError(error.reason);
 
       // 显示结果，跳转页面
       if (result.postExists)
-        alert('This link has already been posted（该链接已经存在）');
-
-      //Router.go('postPage', {_id: result._id});
+        throwError('This link has already been posted（该链接已经存在）');
+    
+      Router.go('postPage', {_id: result._id});
     });
-    Router.go('postsList'); 
   }
 });
